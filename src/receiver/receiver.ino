3001f55bc74f150335d89ae4d3be69f3b24661e5
Timbaleek <timbaleek@hotmail.de>
2018-12-20 16:10:36 +0100

    merged driver and reciever

--- src/receiver/receiver.ino
+++ src/receiver/receiver.ino
@@ -1,20 +1,3 @@
-/*
- Copyright (C) 2012 James Coliz, Jr. <maniacbug@ymail.com>
-
- This program is free software; you can redistribute it and/or
- modify it under the terms of the GNU General Public License
- version 2 as published by the Free Software Foundation.
- 
- Update 2014 - TMRh20
- */
-
-/**
- * Simplest possible example of using RF24Network,
- *
- * RECEIVER NODE
- * Listens for messages from the transmitter and prints them out.
- */
-
 #include <RF24Network.h>
 #include <RF24.h>
 #include <SPI.h>
@@ -29,12 +12,43 @@ const uint16_t other_node = 01;   // Address of the other node in Octal format
 struct payload_t {                 // Structure of our payload
   unsigned long ms;
   unsigned long counter;
-  char mes[2];
+  int mes;
 };
 
+bool leftLast;
+bool rightLast;
+
+int leftCounter;
+int rightCounter;
+
+int rightCorrection;
+
+//INPUT
+const float leftCirc = 23.5; //23.5
+const float rightCirc = 22.8; //22.8
+
+float steerStrength = 0.5;
+const int wheelStp = 155;
+
+bool directionForward = true;
 
 void setup(void)
 {
+  //rotation sensor
+  pinMode(3, INPUT);
+  pinMode(4, INPUT);
+  //left
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  //right
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+
+  leftLast = digitalRead(3);
+  rightLast = digitalRead(4);
+  leftCounter = 0;
+  rightCounter = 0;
+  
   Serial.begin(115200);
   Serial.println("RF24Network/examples/helloworld_rx/");
  
@@ -53,11 +67,76 @@ void loop(void){
     RF24NetworkHeader header;        // If so, grab it and print it out
     payload_t payload;
     network.read(header,&payload,sizeof(payload));
-    Serial.print("Received packet #");
-    Serial.print(payload.counter);
-    Serial.print(" at ");
-    Serial.println(payload.ms);
-    Serial.print(payload.mes[0]);
-    Serial.println(payload.mes[1]);
+    //Serial.print("Received packet #");
+    //Serial.print(payload.counter);
+    //Serial.print(" at ");
+    //Serial.println(payload.ms);
+    Serial.print(payload.mes);
+    drive((int)payload.mes);
   }
 }
+
+void drive(int steer){
+    if (digitalRead(3) != leftLast) {
+    leftCounter += 1;
+    leftLast = !leftLast;
+  }
+  if (digitalRead(4) != rightLast) {
+    rightCounter += 1;
+    rightLast = !rightLast;
+  }
+
+  rightCorrection = (leftCounter * leftCirc - rightCounter * rightCirc);
+
+  //Serial.println((analogRead(A4) - 511));
+  //rightCorrection = *steerStrength;
+
+
+  int rightStp = 0;
+  int leftStp = 0;
+  float potent = analogRead(A4) - 512;
+
+  float steerMultiplierL = 1;
+  float steerMultiplierR = 1;
+
+  if (potent >= -20 and potent <= 20) {
+    if (rightCorrection > 0) {
+      rightStp = wheelStp;
+    } 
+    else if (rightCorrection < 0) {
+      leftStp = wheelStp;
+    } else if (rightCorrection == 0) {
+      leftCounter = 0;
+      rightCounter = 0;
+    }
+  } else {
+    leftStp = 0;
+    rightStp = 0;
+    if(potent < -20){
+      steerMultiplierR = 1-((-potent/512));
+      steerMultiplierL = 1;
+    } else {
+      steerMultiplierL = 1-((potent/512));
+      steerMultiplierR = 1;
+    }
+  }
+
+
+  //Serial.println(rightCounter);
+  //Serial.println(rightCorrection);
+  //Serial.println(potent);
+  //Serial.println(1-abs(potent/512));
+
+  if (directionForward == true) {
+    analogWrite(5, 0);
+    analogWrite(6, (255 - leftStp) * steerMultiplierR); //(((float)analogRead(A4)-512)/512);
+    analogWrite(9, 0);
+    analogWrite(10, (255 - rightStp) * steerMultiplierL);
+  } else {
+    analogWrite(5, 255 - leftStp);
+    analogWrite(6, 0);
+    analogWrite(9, 255 - rightStp);
+    analogWrite(10, 0);
+  }
+}
+
