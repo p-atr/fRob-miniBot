b73ecbca7c28536ce19b848234af77a36a11c1b8
patr_ <30580969+p-atr@users.noreply.github.com>
2019-01-16 08:22:05 +0100

    code cleanup

--- src/Distanzsensor/Distanzsensor.ino
+++ src/Distanzsensor/Distanzsensor.ino
@@ -1,38 +1,36 @@
-// NewPing library gohlt?
-
 #include <NewPing.h>
-int Trigpin = 9;
-int Echopin = 10;
-int maxdistanz = 200;
+const int Trigpin = 9;
+const int Echopin = 10;
+const int maxdistance = 200;
+NewPing sonar(Trigpin, Echopin, maxdistance);
+
 int distanz1;
 int distanz2;
 int speeed;
 int standartspeed; // muss noch bestimmt werden
 int maxentfernig;  // muss noch bestimmt werden
-NewPing sonar(Trigpin, Echopin, maxdistanz);
+
 void setup() {
-  // put your setup code here, to run once:
   pinMode(9, INPUT);
   Serial.begin(19200);
 }
 
 void loop() {
-  // gschwindigkeit bestimme
   
+  // gschwindigkeit bestimme
   distanz1 = sonar.ping_cm();
   //Serial.println(distanz1);
   delay(1000);
   distanz2 = sonar.ping_cm();
   //Serial.println(distanz2);
   speeed= (distanz1-distanz2)/1;
-  Serial.println("gschwindigkeit: "+  speeed);
-  delay(50); 
+  Serial.println("gschwindigkeit: " +  speeed);
+  
   if (distanz1<10 or distanz2<10) {    //nöd znöch a t wand
   Serial.println("LOOOL");
   }
+  
   if (standartspeed*1.1<speeed or standartspeed*0.9>speeed and sonar.ping_cm()<maxentfernig) { // entdeckig vom auto
   Serial.println("Oha! Da ist mir was im weg -> auto entdeckt");
   }
-  
 }
-
--- src/ledStripe/ledStripe.ino
+++ src/ledStripe/ledStripe.ino
@@ -1,21 +1,23 @@
 #include <FastLED.h>
 
 #define NUM_LEDS 7
-
 #define DATA_PIN 3
 #define CLOCK_PIN 13
 
 CRGB leds[NUM_LEDS];
-int colors[7][3] = {{255,0,0},{170,85,0},{85,170,0},{0,255,0},{0,170,85},{0,85,170},{0,0,255}};
+int led_colors[7][3] = {{255,0,0},{170,85,0},{85,170,0},{0,255,0},{0,170,85},{0,85,170},{0,0,255}};
 
-void setup() { 
-      FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
-}
-
-void loop() { 
+void led_setup() {
+  FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
   for(int i = 0; i<NUM_LEDS; i++){
-    leds[i] = CRGB(colors[i][0],colors[i][1],colors[i][2]);
+    leds[i] = CRGB(led_colors[i][0],led_colors[i][1],led_colors[i][2]);
   }
   FastLED.show();
+}
 
+void setup() {
+  led_setup();
+}
+
+void loop() {
 }
