1fb0849d2cbbdd665a8cc6616cfa9276f0b5d6bb
patr_ <your-email@example.com>
2019-05-07 18:15:33 +0200

    working Network

--- src/master/master.ino
+++ src/master/master.ino
@@ -4,10 +4,11 @@
 
 String command;
 
-RF24 radio(7,8);                   // nRF24L01(+) radio attached using Getting Started board 
+RF24 radio(7, 8);                  // nRF24L01(+) radio attached using Getting Started board
 
 RF24Network network(radio);        // Network uses that radio
 const uint16_t this_node = 00;     // Address of our node in Octal format ( 04,031, etc)
+const uint16_t other_node = 01;
 
 struct payload_t {                 // Structure of our payload
   int id ;
@@ -18,25 +19,38 @@ void setup(void)
 {
   Serial.begin(115200);
   Serial.println("MASTER");
- 
+
   SPI.begin();
   radio.begin();
   network.begin(/*channel*/ 90, /*node address*/ this_node);
 }
 
-void loop(void){
+void network_send(int id, uint16_t node) {
+  Serial.print("Sending...");
+  payload_t payload = {id, this_node};
+  RF24NetworkHeader header(/*to node*/ other_node);
+  bool ok = network.write(header, &payload, sizeof(payload));
+  if (ok) {
+    Serial.println("ok.");
+  }
+  else {
+    Serial.println("failed.");
+  }
+}
+
+void loop(void) {
+  network.update();
+  
   if (Serial.available()) {
     Serial.setTimeout(2);
     command = Serial.readString();
-    Serial.println(command);
-  }
-
-  network.update();                   // Check the network regularly
-  while ( network.available() ) {     // Is there anything ready for us?
-    RF24NetworkHeader header;         // If so, grab it and print it out
-    payload_t payload;
-    network.read(header,&payload,sizeof(payload));
-    Serial.print("Received packet from node #");
-    Serial.println(payload.node);
+    if (command == "ping\n") {
+      Serial.println("PING RECEIVED; SENDING TO NODE 01");
+      network_send(0, 01);
+    }
+    else {
+      Serial.print("UNKNOWN COMMAND: ");
+      Serial.println(command);
+    }
   }
 }
--- src/slave/slave.ino
+++ src/slave/slave.ino
@@ -1,34 +1,23 @@
 /*
- Copyright (C) 2012 James Coliz, Jr. <maniacbug@ymail.com>
+  Copyright (C) 2012 James Coliz, Jr. <maniacbug@ymail.com>
 
- This program is free software; you can redistribute it and/or
- modify it under the terms of the GNU General Public License
- version 2 as published by the Free Software Foundation.
- 
- Update 2014 - TMRh20
- */
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  version 2 as published by the Free Software Foundation.
 
-/**
- * Simplest possible example of using RF24Network 
- *
- * TRANSMITTER NODE
- * Every 2 seconds, send a payload to the receiver node.
- */
+  Update 2014 - TMRh20
+*/
 
 #include <RF24Network.h>
 #include <RF24.h>
 #include <SPI.h>
 
-RF24 radio(7,8);                    // nRF24L01(+) radio attached using Getting Started board 
+RF24 radio(7, 8);                   // nRF24L01(+) radio attached using Getting Started board
 
-RF24Network network(radio);          // Network uses that radio
+RF24Network network(radio);         // Network uses that radio
 
-const uint16_t this_node = 02;        // Address of our node in Octal format
-const uint16_t master = 03;       // Address of the other node in Octal format
-
-const unsigned long interval = 2000; //ms  // How often to send 'hello world to the other unit
-
-unsigned long last_sent;             // When did we last send?
+const uint16_t this_node = 01;      // Address of our node in Octal format
+const uint16_t master = 00;         // Address of the other node in Octal format
 
 struct payload_t {                  // Structure of our payload
   int id;
@@ -39,29 +28,42 @@ void setup(void)
 {
   Serial.begin(115200);
   Serial.println("SLAVE");
- 
+
   SPI.begin();
   radio.begin();
   network.begin(/*channel*/ 90, /*node address*/ this_node);
 }
 
-void loop() {
-  
-  network.update();                          // Check the network regularly
-
-  
-  unsigned long now = millis();              // If it's time to send a message, send it!
-  if ( now - last_sent >= interval  )
-  {
-    last_sent = now;
-
-    Serial.print("Sending...");
-    payload_t payload = {0, this_node};
-    RF24NetworkHeader header(/*to node*/ master);
-    bool ok = network.write(header,&payload,sizeof(payload));
-    if (ok)
-      Serial.println("ok.");
-    else
-      Serial.println("failed.");
+void network_receive() {
+  while (network.available() ) {          // Is there anything ready for us?
+    RF24NetworkHeader header;             // If so, grab it and print it out
+    payload_t payload;
+    network.read(header, &payload, sizeof(payload));
+    if (payload.id == 0) {
+      Serial.print("Received packet from node #");
+      Serial.println(payload.node);
+    }
+    else {
+      Serial.print("UNKNOWN PACKET FROM NODE #");
+      Serial.println(payload.node);
+    }
   }
+}
+
+void network_send(int id, uint16_t node) {
+  Serial.print("Sending...");
+  payload_t payload = {id, this_node};
+  RF24NetworkHeader header(/*to node*/ node);
+  bool ok = network.write(header, &payload, sizeof(payload));
+  if (ok) {
+    Serial.println("ok.");
+  }
+  else {
+    Serial.println("failed.");
+  }
+}
+
+void loop() {
+  network.update();                       // Check the network regularly
+  network_receive();
 }
